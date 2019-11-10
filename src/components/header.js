e2692c5aae4a0183236e783e84ddf3d2f1ba28cc
Brian <a.gray.cloud@gmail.com>
2019-11-10 03:11:40 -0400

    more icon tweaking, need to figure out how to transform children icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCopy,
+  faAddressCard,
+  faTasks
+} from "@fortawesome/free-solid-svg-icons";
 
 export default class Header extends React.Component {
   render() {
@@ -7,12 +13,19 @@ export default class Header extends React.Component {
         {/** Header Item Components Here */}
         <div className="header-item">
           <p className="grow">Projects</p>
+          <FontAwesomeIcon icon={faTasks} className="grow icon" size="3x" />
         </div>
         <div className="header-item">
           <p className="grow">Contact</p>
+          <FontAwesomeIcon
+            icon={faAddressCard}
+            className="grow icon"
+            size="3x"
+          />
         </div>
         <div className="header-item">
           <p className="grow">Resume</p>
+          <FontAwesomeIcon icon={faCopy} className="grow icon" size="3x" />
         </div>
       </div>
     );
