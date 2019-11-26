f60c90dd7d2c3b43706a26c7022bb1a36a1d8778
Brian <a.gray.cloud@gmail.com>
2019-11-26 17:15:52 -0400

    might as well make carousel usestate too

--- src/components/Layout/Carousel.js
+++ src/components/Layout/Carousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
@@ -6,61 +6,96 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { ImageModal } from "./index";
 
-class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: this.props.imgs,
-      active: 0
-    };
-  }
+const Carousel = ({ images, show, hideModal, showModal }) => {
+  const [active, setActive] = useState(0);
 
-  setActive = idx => {
-    this.setState({ active: idx });
-  };
+  return (
+    <React.Fragment>
+      <ImageModal show={show} handleClose={hideModal} imgSrc={images[active]} />
+      <FontAwesomeIcon
+        icon={faChevronLeft}
+        size="lg"
+        onClick={() => {
+          let idx = active - 1;
+          if (idx < 0) idx = images.length - 1;
+          setActive(idx);
+        }}
+      />
+      <img
+        className="card-img-top"
+        style={{ cursor: "zoom-in" }}
+        src={images[active]}
+        alt="Something wrong"
+        onClick={showModal}
+      />
+      <FontAwesomeIcon
+        icon={faChevronRight}
+        size="lg"
+        onClick={() => {
+          let idx = active + 1;
+          if (idx >= images.length) idx = 0;
+          setActive(idx);
+        }}
+      />
+    </React.Fragment>
+  ); //
+};
 
-  prev = () => {
-    let idx = this.state.active - 1;
-    if (idx < 0) {
-      idx = this.images.length - 1;
-    }
+// class Carousel extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       images: this.props.imgs,
+//       active: 0
+//     };
+//   }
 
-    this.setActive(idx);
-  };
+//   setActive = idx => {
+//     this.setState({ active: idx });
+//   };
 
-  next = () => {
-    let idx = this.state.active + 1;
-    if (idx >= this.images.length) {
-      idx = 0;
-    }
+//   prev = () => {
+//     let idx = this.state.active - 1;
+//     if (idx < 0) {
+//       idx = this.images.length - 1;
+//     }
 
-    this.setActive(idx);
-  };
+//     this.setActive(idx);
+//   };
 
-  render() {
-    return (
-      <div>
-        <ImageModal
-          show={this.props.show}
-          handleClose={this.props.handleClose}
-          imgSrc={this.state.images[this.state.active]}
-        />
-        <FontAwesomeIcon icon={faChevronLeft} size="lg" onClick={this.prev()} />
-        <img
-          className="card-img-top"
-          style={{ cursor: "zoom-in" }}
-          src={this.state.images[this.state.active]}
-          alt="Something wrong"
-          onClick={this.props.showModal}
-        />
-        <FontAwesomeIcon
-          icon={faChevronRight}
-          size="lg"
-          onClick={this.next()}
-        />
-      </div>
-    ); //
-  }
-}
+//   next = () => {
+//     let idx = this.state.active + 1;
+//     if (idx >= this.images.length) {
+//       idx = 0;
+//     }
+
+//     this.setActive(idx);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <ImageModal
+//           show={this.props.show}
+//           handleClose={this.props.handleClose}
+//           imgSrc={this.state.images[this.state.active]}
+//         />
+//         <FontAwesomeIcon icon={faChevronLeft} size="lg" onClick={this.prev()} />
+//         <img
+//           className="card-img-top"
+//           style={{ cursor: "zoom-in" }}
+//           src={this.state.images[this.state.active]}
+//           alt="Something wrong"
+//           onClick={this.props.showModal}
+//         />
+//         <FontAwesomeIcon
+//           icon={faChevronRight}
+//           size="lg"
+//           onClick={this.next()}
+//         />
+//       </div>
+//     ); //
+//   }
+// }
 
 export default Carousel;
