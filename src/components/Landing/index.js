19004e4c862196acf6f731e81912b5189cf89573
Brian <a.gray.cloud@gmail.com>
2019-12-11 17:50:29 -0400

    remove unused state

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -6,8 +6,6 @@ let me =
   "https://scontent.fewr1-2.fna.fbcdn.net/v/t1.15752-9/s2048x2048/76618293_549823525585525_5234997996277465088_n.jpg?_nc_cat=111&_nc_oc=AQlSpP7Y3-nEWSNFOL6CPlVhegz0FMZtAV0VOeQLfC_HX20xm6bD2k2kcmVVjjvjEkg&_nc_ht=scontent.fewr1-2.fna&oh=4c66c2eaf36cc4a1cda6139f7c4509fa&oe=5E58DA07";
 
 const Landing = () => {
-  const [show, setShow] = useState(false);
-
   return (
     <div className="main">
       <ContainerSmall where="left">
@@ -16,13 +14,7 @@ const Landing = () => {
         <p>I'm using this as a playground to try out front-end things</p>
         <p>Me messing with python: </p>
         <div id="me-container">
-          <img
-            className="me"
-            src={me}
-            alt="me"
-            width="400"
-            onClick={() => setShow(true)}
-          />
+          <img className="me" src={me} alt="me" width="400" />
         </div>
       </ContainerSmall>
       <RightBox />
