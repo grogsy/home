0f814b9a45a505e08c3ac8120c2f9c39dca39236
Brian <a.gray.cloud@gmail.com>
2019-12-12 01:46:31 -0400

    create a link parsing component

---
+++ src/components/Layout/LinkParser.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LinkParser = ({ link, children, extraClasses }) => {
+  let className = "react-link";
+  if (extraClasses) {
+    className = className + " " + extraClasses;
+  }
+
+  return link && link.startsWith("http") ? (
+    <a target="_blank" href={link} className={className}>
+      {children}
+    </a>
+  ) : (
+    <Link to={link ? `/${link}` : "/"} className={className}>
+      {children}
+    </Link>
+  );
+};
+
+export default LinkParser;
