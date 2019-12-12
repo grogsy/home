a46e9f0bda6552f54ee29b2adc8a6b69628a528f
Brian <a.gray.cloud@gmail.com>
2019-12-12 03:19:30 -0400

    remove unused component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Landing from "./Landing";
 import Projects from "./Projects";
-import ContactMe from "./Contact";
 import Resume from "./Resume";
 
 const Routes = props => {
   return (
     <Switch>
       <Route path="/projects" component={Projects} />
-      <Route path="/contact" component={ContactMe} />
       <Route path="/resume" component={Resume} />
       <Route component={Landing} />
     </Switch>
