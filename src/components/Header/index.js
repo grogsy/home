56e526c6a195704a73155d5d20e008a9a017b6ae
Brian <a.gray.cloud@gmail.com>
2019-12-12 01:51:10 -0400

    change header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,27 @@
 import React from "react";
-import {
-  faCopy,
-  faAddressCard,
-  faTasks,
-  faHome
-} from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faTasks, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import HeaderItem from "./HeaderItem";
 
 const Header = ({ className }) => {
   return (
-    <div className={className}>
+    <nav className="navbar fixed-top navbar-dark bg-dark">
       <HeaderItem headerName="Home" icon={faHome} />
       <HeaderItem headerName="Projects" icon={faTasks} link="projects" />
-      <HeaderItem headerName="Contact" icon={faAddressCard} link="contact" />
       <HeaderItem headerName="Resume" icon={faCopy} link="resume" />
-    </div>
+      <HeaderItem
+        headerName="GitHub"
+        icon={faGithub}
+        link="https://github.com/grogsy"
+      />
+      <HeaderItem
+        headerName="Linkedin"
+        icon={faLinkedin}
+        link="linkedin"
+        link="https://linkedin.com/brian2772"
+      />
+    </nav>
   );
 };
 
