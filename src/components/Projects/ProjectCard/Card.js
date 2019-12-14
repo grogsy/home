499977537afbb7477b68f87e6b67a0897d90805a
Brian <a.gray.cloud@gmail.com>
2019-12-14 17:46:52 -0400

    remove inline style

--- src/components/Projects/ProjectCard/Card.js
+++ src/components/Projects/ProjectCard/Card.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 const ProjectCard = ({ isClosed, children }) => {
-  return (
-    <div
-      className={isClosed ? "card" : "card fade-in"}
-      style={{ width: "30rem" }}
-    >
-      {children}
-    </div>
-  );
+  return <div className={isClosed ? "card" : "card fade-in"}>{children}</div>;
 };
 
 export default ProjectCard;
