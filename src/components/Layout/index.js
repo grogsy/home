9bdb5299b1707ac44ed7d8b1ae62f674252e6d10
Brian <a.gray.cloud@gmail.com>
2019-12-14 02:53:41 -0400

    export link parser default from layout folder

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,3 +1,4 @@
 export { default as ContainerSmall } from "./ContainerSmaller";
 export { default as ImageModal } from "./ImageModal";
 export { default as Carousel } from "./Carousel";
+export { default as Link } from "./LinkParser";
